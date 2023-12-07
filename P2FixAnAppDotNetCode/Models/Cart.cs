101dbef1dace845a6bf993f986abb21a6a6679a5
Samir Hilali <samir.hilali@epitech.eu>
2023-12-07 21:00:30 +0100

    fix FindProductInCartLines

--- P2FixAnAppDotNetCode/Models/Cart.cs
+++ P2FixAnAppDotNetCode/Models/Cart.cs
@@ -79,8 +79,7 @@ namespace P2FixAnAppDotNetCode.Models
         /// </summary>
         public Product FindProductInCartLines(int productId)
         {
-            // TODO implement the method
-            return null;
+            return GetCartLineList().FirstOrDefault(p => p.Product.Id == productId)?.Product;
         }
 
         /// <summary>
